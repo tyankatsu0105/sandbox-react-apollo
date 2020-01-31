ec409d30bf0bd064c9d7c5c96339813a1c28dd6c
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2020-01-31 10:04:06 +0900

    add material-ui, apollo, and react-router

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './app/app';
+import { client } from './api';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { customTheme } from './styles/material-ui/customTheme';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import { App } from './app/app';
+
+import { environment } from './environments/environment';
+
+console.log(environment);
+
+ReactDOM.render(
+    <BrowserRouter>
+          <App />
+    </BrowserRouter>,
+  document.getElementById('root')
+);
+// ReactDOM.render(
+//   <ApolloProvider client={client}>
+//     <BrowserRouter>
+//       <ThemeProvider theme={customTheme}>
+//         <CssBaseline />
+//           <App />
+//       </ThemeProvider>
+//     </BrowserRouter>
+//   </ApolloProvider>,
+//   document.getElementById('root')
+// );
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  testRegex: '(/__test__/.*|(\\.|/)spec)\\.tsx?$',
   transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest'
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+    '^.+\\.graphql$': 'jest-transform-graphql',
   },
+  testURL: 'http://localhost',
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
-  passWithNoTests: true
+  passWithNoTests: true,
 };

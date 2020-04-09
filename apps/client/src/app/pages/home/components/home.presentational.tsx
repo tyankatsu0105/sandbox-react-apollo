85df4b4aa90564bccbde29f0bbbf403e801fcba8
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-04-09 22:24:06 +0900

    add external link

--- apps/client/src/app/pages/home/components/home.presentational.tsx
+++ apps/client/src/app/pages/home/components/home.presentational.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from 'react';
-import { Box, Link } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+
+import { ExternalLink } from '@sandbox-react-apollo/components';
 
 type Props = {};
 
@@ -7,13 +9,14 @@ export const HomePresentational: FunctionComponent<Props> = (props) => {
   return (
     <Box>
       <p>
-        <Link href="https://www.apollographql.com/docs/react/">
+        <ExternalLink href="https://www.apollographql.com/docs/react/">
           Apollo client
-        </Link>
+        </ExternalLink>
         の技術検証用に作ったものです。
       </p>
       <p>
-        スタイルは <Link href="https://material-ui.com/">MATERIAL-UI</Link>
+        スタイルは
+        <ExternalLink href="https://material-ui.com/">MATERIAL-UI</ExternalLink>
         を使ってます。
       </p>
     </Box>

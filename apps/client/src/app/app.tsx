32d9c5e4d7d739a9245839154b81a3ff57762c97
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-04-09 21:56:06 +0900

    add pageloader

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -2,6 +2,8 @@ import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router';
 import { Helmet } from 'react-helmet';
 
+import { PageLoader } from '@sandbox-react-apollo/components';
+
 import { LoginPage } from './pages/login';
 import { Authentication } from './authentication';
 import { Authenticated } from './route';
@@ -12,7 +14,7 @@ export const App = () => (
   <>
     <Helmet titleTemplate={`%s | ${SITE_NAME}`} />
 
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<PageLoader />}>
       <Switch>
         <Route exact path="/login" component={LoginPage} />
         <Authentication path="/" component={Authenticated} />
--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/paper';
+export * from './lib/loader';
